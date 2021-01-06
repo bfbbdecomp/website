ba9bba9a6cbd45059116417adaf3b54f409a3832
Matt <5638426+mattbruv@users.noreply.github.com>
2021-01-05 21:57:34 -0500

    tweak

--- python/files.py
+++ python/files.py
@@ -1,15 +1,18 @@
 import json
 
+
 def addressToInt(address):
     return int(address, 16)
+
 
 def getFileInfo():
     return json.loads(open("fileInfo.json").read())
 
+
 def getFunctionFile(files, address):
     address = addressToInt(address)
     for f in files:
-        name = f["name"]
+        name = f["path"]
         start = addressToInt(f["start"])
         end = addressToInt(f["end"])
         if address >= start and address < end:
