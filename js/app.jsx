8851a8e3e841ac0d2d82d30a9d912a9acafcbd94
Matt <5638426+mattbruv@users.noreply.github.com>
2021-01-09 22:31:27 -0500

    idk wtf i'm doing

--- js/app.jsx
+++ js/app.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Header from "./header";
+import { Grid } from "@material-ui/core";
 
 export default class App extends React.Component {
   render() {
     const Page = this.props.page;
     return (
-      <div>
-        <Header />
-        <Page />
-      </div>
+      <Grid container direction="column">
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item container>
+          <Page />
+        </Grid>
+      </Grid>
     );
   }
 }
--- js/header.jsx
+++ js/header.jsx
@@ -1,13 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Link } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link as RouterLink } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  headerLink: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 export default class Header extends React.Component {
   render() {
+    const classes = useStyles();
+
     return (
-      <div>
-        <Link to="/progress">Progress</Link>
-        <Link to="/faq">FAQ</Link>
-      </div>
+      <AppBar position="static" color="transparent">
+        <Toolbar variant="dense">
+          <Typography className={classes.headerLink} variant="h6">
+            <Link component={RouterLink} to="/progress">
+              Progress
+            </Link>
+          </Typography>
+          <Typography variant="h6">
+            <Link component={RouterLink} to="/faq">
+              FAQ
+            </Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
