d605b3e9d316bc9dcf5183192c70eb06a8924267
Matt <5638426+mattbruv@users.noreply.github.com>
2021-01-10 12:15:01 -0500

    blah

--- js/header.jsx
+++ js/header.jsx
@@ -1,12 +1,24 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Link } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Link,
+  Tabs,
+  Tab,
+} from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 
 export default class Header extends React.Component {
   render() {
     return (
-      <AppBar position="static" color="transparent" elevation="3">
+      <AppBar position="static" color="transparent" elevation={3}>
         <Toolbar variant="dense">
+          {/*
+          <Tabs value={0}>
+            <Tab value={0} label="Item One" to="/progress" component={Link} />
+          </Tabs>
+          */}
           <Typography variant="h6">
             <Link component={RouterLink} to="/progress">
               Progress
