c22058b4be58565a13b9ad5eb10ed837ce9ab2c5
Matt <5638426+mattbruv@users.noreply.github.com>
2021-01-10 11:06:54 -0500

    get rid of broken style thing

--- js/header.jsx
+++ js/header.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Link } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import { Link as RouterLink } from "react-router-dom";
-
-const useStyles = makeStyles((theme) => ({
-  headerLink: {
-    marginRight: theme.spacing(2),
-  },
-}));
 
 export default class Header extends React.Component {
   render() {
-    const classes = useStyles();
-
     return (
-      <AppBar position="static" color="transparent">
+      <AppBar position="static" color="transparent" elevation="3">
         <Toolbar variant="dense">
-          <Typography className={classes.headerLink} variant="h6">
+          <Typography variant="h6">
             <Link component={RouterLink} to="/progress">
               Progress
             </Link>
