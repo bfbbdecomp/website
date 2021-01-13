5b04657783c92a32c10221e2886c4ce90ff52ad0
Joey Ballentine <joeyjballentine@gmail.com>
2021-01-13 03:29:57 -0600

    Dynamically load spatulas

--- js/progress/bfbb-menu/menu-components/spatula-selector/spatula-selector.jsx
+++ js/progress/bfbb-menu/menu-components/spatula-selector/spatula-selector.jsx
@@ -3,6 +3,8 @@ import "./spatula-selector.css";
 
 import Spatula from "./spatula";
 
+const spatulas = require('../../spatulas.json');
+
 export default class SpatulaSelector extends React.Component {
 
   constructor(props) {
@@ -13,43 +15,47 @@ export default class SpatulaSelector extends React.Component {
     };
   }
 
+  upJellyfishClick = () => {
+    this.setState((state, props) => ({
+      selectedArea: state.selectedArea > 0 ? state.selectedArea - 1 : spatulas.length - 1
+    }))
+  }
+
+  downJellyfishClick = () => {
+    this.setState((state, props) => ({
+      selectedArea: state.selectedArea < spatulas.length - 1 ? state.selectedArea + 1 : 0
+    }))
+  }
+
+  onSpatulaHover = (index) => {
+    this.setState((state, props) => ({
+      selectedSpatula: index
+    }))
+  }
+
 
   render() {
     return (
       <>
         <div className="area-selector">
           <div className="inner-selector">
-            <div className="jellyfish" id="jellyfish-up"></div>
-            <div className="area-bubble" id="area-bubble-selected"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="area-bubble"></div>
-            <div className="jellyfish" id="jellyfish-down"></div>
+            <div className="jellyfish" id="jellyfish-up" onClick={() => {this.upJellyfishClick()}}></div>
+            {spatulas.map((area, index)=>(
+              <div className="area-bubble" id={this.state.selectedArea === index ? 'area-bubble-selected' : ''} key={index}></div>
+            ))}
+            <div className="jellyfish" id="jellyfish-down" onClick={() => {this.downJellyfishClick()}}></div>
           </div>
         </div>
         <div className="spatula-selector">
-          <p id="area-title">Bikini Bottom</p>
+          <p id="area-title">{spatulas[this.state.selectedArea].level}</p>
           <div className="spatula-container">
-            <Spatula gold={true}></Spatula>
-            <Spatula gold={true}></Spatula>
-            <Spatula></Spatula>
-            <Spatula></Spatula>
-            <Spatula></Spatula>
-            <Spatula></Spatula>
-            <Spatula></Spatula>
-            <Spatula></Spatula>
+            {spatulas[this.state.selectedArea].spatulas.map((spatula, index) => (
+              <div key={index} onMouseOver={() => {this.onSpatulaHover(index)}}>
+                <Spatula key={index}></Spatula>
+              </div>
+            ))}
           </div>
-          <p id="spatula-name">On top of the pineapple</p>
+          <p id="spatula-name">{spatulas[this.state.selectedArea].spatulas[this.state.selectedSpatula]}</p>
         </div>
       </>
     );
