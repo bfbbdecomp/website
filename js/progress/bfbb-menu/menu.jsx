245e8140794ffc5b6661ce3361303d351ef0b6b0
Joey Ballentine <joeyjballentine@gmail.com>
2021-01-13 16:35:10 -0600

    Move contributors to prop

--- js/progress/bfbb-menu/menu.jsx
+++ js/progress/bfbb-menu/menu.jsx
@@ -19,38 +19,7 @@ export default class Menu extends React.Component {
   }
 
   render() {
-    const contributors = [
-      {
-        name: "pslehisl",
-        profileImg: "https://github.com/identicons/pslehisl.png",
-        profileUrl: "https://github.com/pslehisl",
-      },
-      {
-        name: "mattbruv",
-        profileImg:
-          "https://avatars0.githubusercontent.com/u/5638426?s=460&u=1c4a9f676e848e4edc7da2832c35d3d99bedc780&v=4",
-        profileUrl: "https://github.com/mattbruv",
-      },
-      {
-        name: "stravant",
-        profileImg:
-          "https://avatars2.githubusercontent.com/u/2074518?s=460&u=997bb336a3890e3b0e33b61913e4cd17d7bd55a5&v=4",
-        profileUrl: "https://github.com/stravant",
-      },
-      {
-        name: "DarkRTA",
-        profileImg:
-          "https://avatars2.githubusercontent.com/u/15368682?s=460&u=b0cc63f5bf5bc25b5637c32c7cbc2dbf8f95af08&v=4",
-        profileUrl: "https://github.com/DarkRTA",
-      },
-      {
-        name: "Gota7",
-        profileImg:
-          "https://avatars0.githubusercontent.com/u/22335998?s=460&u=f5cf38fa26e2a66eb662b111f554a4b9d2624dc0&v=4",
-        profileUrl: "https://github.com/Gota7",
-      },
-    ];
-
+    const contributors = this.props.contributors;
     const spatulaCount = this.props.spatulaCount;
     const shinyCount = this.props.shinyCount;
     const sockCount = this.props.sockCount;
@@ -60,15 +29,6 @@ export default class Menu extends React.Component {
     const commitCount = this.props.commitCount;
     const contributorCount = this.props.contributorCount;
     const pullRequestCount = this.props.pullRequestCount;
-
-    // const spatulaCount = 5;
-    // const shinyCount = 20274;
-    // const sockCount = 2;
-    // const collectableCount = 25;
-    // const collectableTotal = 121;
-    // const commitCount = 174;
-    // const contributorCount = 8;
-    // const pullRequestCount = 1;
 
     return (
       <div className="container">
--- js/progress/progress.jsx
+++ js/progress/progress.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import Menu from "./bfbb-menu/menu";
 
+const contributors = [
+  {
+    name: "pslehisl",
+    profileImg: "https://github.com/identicons/pslehisl.png",
+    profileUrl: "https://github.com/pslehisl",
+  },
+  {
+    name: "mattbruv",
+    profileImg:
+      "https://avatars0.githubusercontent.com/u/5638426?s=460&u=1c4a9f676e848e4edc7da2832c35d3d99bedc780&v=4",
+    profileUrl: "https://github.com/mattbruv",
+  },
+  {
+    name: "stravant",
+    profileImg:
+      "https://avatars2.githubusercontent.com/u/2074518?s=460&u=997bb336a3890e3b0e33b61913e4cd17d7bd55a5&v=4",
+    profileUrl: "https://github.com/stravant",
+  },
+  {
+    name: "DarkRTA",
+    profileImg:
+      "https://avatars2.githubusercontent.com/u/15368682?s=460&u=b0cc63f5bf5bc25b5637c32c7cbc2dbf8f95af08&v=4",
+    profileUrl: "https://github.com/DarkRTA",
+  },
+  {
+    name: "Gota7",
+    profileImg:
+      "https://avatars0.githubusercontent.com/u/22335998?s=460&u=f5cf38fa26e2a66eb662b111f554a4b9d2624dc0&v=4",
+    profileUrl: "https://github.com/Gota7",
+  },
+];
+
 export default class Progress extends React.Component {
   render() {
     return (
       <>
         <div>Progress Page!</div>
         <Menu
+          contributors={contributors}
           spatulaCount={5}
           shinyCount={20274}
           sockCount={2}
