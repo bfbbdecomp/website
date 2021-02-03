545694e218ebe47d889602ce9643c6b759694694
Matt <5638426+mattbruv@users.noreply.github.com>
2021-02-03 11:30:38 -0500

    add build script

---
+++ build.py
@@ -0,0 +1,35 @@
+import subprocess
+import os
+
+path = os.path.dirname(os.path.realpath(__file__))
+
+commands = [  #
+    {
+        "cwd":
+        path,
+        "cmd": [  #
+            ["pip", "install", "gitpython"],
+            ["npm", "install", "-g", "parcel-bundler"]
+        ]
+    },
+    {
+        "cwd": path + "/python/",
+        "cmd": [
+            ["python", "process.py"],
+        ]
+    },  #
+    {
+        "cwd": path,
+        "cmd": [
+            ["npm", "install"],
+            ["npm", "run", "build"],
+        ]
+    }
+]
+
+for c in commands:
+    cwd = c["cwd"]
+    cmds = c["cmd"]
+    for cmd in cmds:
+        print(cwd, ' '.join(cmd))
+        subprocess.run(cmd, cwd=cwd, shell=True)
