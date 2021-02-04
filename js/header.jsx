8e9a1ddeb5384b2868c6f3525878a8cbc2f3a9f7
Joey Ballentine <joeyjballentine@gmail.com>
2021-02-04 01:00:21 -0600

    Made the header look good and function properly

--- js/header.jsx
+++ js/header.jsx
@@ -1,27 +1,45 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Link } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Link, BottomNavigation, BottomNavigationAction } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
 
+import GitHubIcon from '@material-ui/icons/GitHub';
+import SportsEsportsIcon from '@material-ui/icons/SportsEsports';
+import TimelineIcon from '@material-ui/icons/Timeline';
+import HelpIcon from '@material-ui/icons/Help';
+
 export default class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: window.location.pathname };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event, newValue) => {
+    this.setState({value: newValue}, () => {
+      if (this.state.value === "github") {
+        window.location.href = "https://www.github.com/bfbbdecomp/"
+      }
+      else if (this.state.value === "discord") {
+        window.location.href = "https://discord.gg/zEr6u3mRWe"
+      }
+    });
+  };
+
   render() {
     return (
       <AppBar position="static" color="transparent" elevation={3}>
-        <Toolbar variant="dense">
-          <Typography variant="h6">
-            <Link component={RouterLink} to="/progress">
-              Progress
-            </Link>
-          </Typography>
-          <Typography variant="h6">
-            <Link component={RouterLink} to="/faq">
-              FAQ
-            </Link>
-          </Typography>
-          <div>
-            <a href="#">GitHub</a>
-            <a href="#">Discord</a>
-          </div>
-        </Toolbar>
+        <BottomNavigation
+          value={this.state.value}
+          onChange={this.handleChange}
+          showLabels
+          className="bottom-navigation"
+        >
+          <BottomNavigationAction component={RouterLink} to="/progress" value="progress" label="Progress" icon={<TimelineIcon />} />
+          <BottomNavigationAction component={RouterLink} to="/faq" value="faq" label="FAQ" icon={<HelpIcon />} />
+          <BottomNavigationAction value="github" label="GitHub" icon={<GitHubIcon />} />
+          <BottomNavigationAction value="discord" label="Discord" icon={<SportsEsportsIcon />} />
+        </BottomNavigation>
       </AppBar>
     );
   }
