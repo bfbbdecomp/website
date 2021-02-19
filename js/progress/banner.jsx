0185c6ef5efc7b709e9eb80b715b351c6fb87ae8
Matt <5638426+mattbruv@users.noreply.github.com>
2021-02-18 22:36:12 -0500

    bar

--- js/progress/banner.jsx
+++ js/progress/banner.jsx
@@ -19,13 +19,13 @@ export default class ProgressBanner extends React.Component {
 
   render() {
     return (
-      <div className="p-4">
+      <div className="md:p-4">
         <div className="text-3xl font-bold text-gray-700 pb-2 md:pb-0">
           Battle for Bikini Bottom is{" "}
           <span className="">{this.percentDone()}%</span> decompiled
         </div>
-        <div className="text-xl text-gray-600">
-          Estimated completion date:{" "}
+        <div className="text-xl font-semibold text-gray-500">
+          Estimated completion Date:{" "}
           <span
             style={{
               cursor: "help",
@@ -35,6 +35,23 @@ export default class ProgressBanner extends React.Component {
           >
             January 69th, 2020
           </span>
+        </div>
+        <div className="my-4">
+          <div className="overflow-hidden h-3 text-xs flex rounded bg-gray-200">
+            <div
+              style={{ width: this.props.repoState.linesPercent + "%" }}
+              className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-500"
+            ></div>
+            <div
+              style={{
+                width:
+                  this.props.repoState.funcsPercent -
+                  this.props.repoState.linesPercent +
+                  "%",
+              }}
+              className="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-200"
+            ></div>
+          </div>
         </div>
       </div>
     );
