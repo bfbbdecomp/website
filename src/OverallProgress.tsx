5d488a91f1631eaa3f121d20ae5e333d8618cc32
Matt P <5638426+mattbruv@users.noreply.github.com>
2024-06-30 16:49:19 -0500

    move file metric info to separate file to reduce clutter

--- src/OverallProgress.tsx
+++ src/OverallProgress.tsx
@@ -7,62 +7,7 @@ import { FileHeatmap } from "./FileHeatmap";
 import "./css/app.css";
 import { SourceFileInfo } from "./File";
 import { useState } from "react";
-import chroma from "chroma-js";
-
-export type FileMetricData = {
-  description: string;
-  value: (unit: Unit) => number;
-  percentage?: (unit: Unit, units: Unit[]) => number;
-  gradient: chroma.Scale;
-};
-
-enum FileMetric {
-  FuzzyPercent = "fuzzy",
-  MatchedPercent = "matched-percent",
-  MatchedCode = "matched",
-  AvgFunctionSize = "avgFnSize",
-  CodeSize = "size",
-}
-
-// const sum = (arr: number[]) => arr.reduce((acc, curr) => acc + curr, 0);
-const max = (arr: number[]) => Math.max(...arr);
-
-const metricData: Record<FileMetric, FileMetricData> = {
-  [FileMetric.FuzzyPercent]: {
-    description: "Close Match %",
-    value: (unit) => unit.fuzzy_match_percent,
-    gradient: chroma.scale(["red", "green"]),
-  },
-  [FileMetric.MatchedPercent]: {
-    description: "Perfect Match %",
-    value: (unit) => (unit.matched_code / unit.total_code) * 100,
-    gradient: chroma.scale(["red", "green"]),
-  },
-  [FileMetric.MatchedCode]: {
-    description: "Perfect Match Size",
-    value: (unit) => unit.matched_code,
-    percentage: (unit, units) =>
-      (unit.matched_code / max(units.flatMap((x) => x.matched_code))) * 100,
-    gradient: chroma.scale(["pink", "darkblue"]),
-  },
-  [FileMetric.CodeSize]: {
-    description: "Code Size",
-    value: (unit) => unit.total_code,
-    percentage: (unit, units) =>
-      (unit.total_code / max(units.flatMap((x) => x.total_code))) * 100,
-    gradient: chroma.scale(["pink", "darkblue"]),
-  },
-  [FileMetric.AvgFunctionSize]: {
-    description: "Average Function Size",
-    value: (unit) => Math.floor(unit.total_code / unit.total_functions),
-    percentage: (unit, units) =>
-      (unit.total_code /
-        unit.total_functions /
-        max(units.flatMap((x) => x.total_code / x.total_functions))) *
-      100,
-    gradient: chroma.scale(["pink", "darkblue"]),
-  },
-};
+import { FileMetric, metricData } from "./FileMetric";
 
 export function OverallProgress() {
   const total = ProgressReport.matched_code_percent;
